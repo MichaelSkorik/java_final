d2bd7045771b8b6cada34f652ed932746181009c
Michael <Michael@SKorik.com>
2023-11-27 17:35:00 +0300

    commit

---
+++ create_java/create5/Human.java
@@ -0,0 +1,33 @@
+package create_java.create5;
+
+public class Human extends Actor {
+
+    @Override
+    public String getName() {
+        return super.name;
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        return super.isMakeOrder;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return super.isTakeOrder;
+    }
+
+    @Override
+    public void setMakeOrder(boolean makeOrder) {
+        super.isMakeOrder = makeOrder;
+    }
+
+    @Override
+    public void setTakeOrder(boolean takeOrder) {
+        super.isTakeOrder = takeOrder;
+    }
+    
+    public Human(String name) {
+        super(name);
+    }
+}
