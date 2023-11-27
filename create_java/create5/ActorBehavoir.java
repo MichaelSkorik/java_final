5594f992b74877f354fb904a974abfa751a13633
Michael <Michael@SKorik.com>
2023-11-27 17:34:43 +0300

    commit

---
+++ create_java/create5/ActorBehavoir.java
@@ -0,0 +1,11 @@
+package create_java.create5;
+
+public interface ActorBehavoir {
+
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean takeOrder);
+    
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+    
+}
